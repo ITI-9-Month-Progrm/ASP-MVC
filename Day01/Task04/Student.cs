922b3ae31252664ed8dea9a3f8226a9fa04423f1
unknown <asmaakhaled603@gmail.com>
2021-03-28 16:52:59 +0200

    Add Tasks of Day01 but task03 has problem

---
+++ Day01/Task04/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task04
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public int grade { get; set; }
+    }
+}
